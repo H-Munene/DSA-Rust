36a91f83502d86706e91fcbc8b110c8f27ac6f12
Hezekiah Munene <hezemunene01@gmail.com>
2024-02-20 22:06:36 +0300

    implemented a recursive binary search function

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,6 +1,8 @@
 /*
     binary search function implementing trait bounds and generic types
  */
+
+//iterative implementation
 pub fn bin_search<T> (data : &[T], target : T) -> Option<usize>
     where T : Copy + PartialOrd
 {
@@ -23,5 +25,25 @@ pub fn bin_search<T> (data : &[T], target : T) -> Option<usize>
         }
     }
 
+    None
+}
+
+//recursive implementation
+
+pub fn recursive_bin_search<T>(haystack : &[T], needle : T, low: usize, high : usize) -> Option<usize>
+    where T : Copy + PartialOrd + PartialEq
+{
+    while low <= high {
+
+        let mid = low + (high - low) /2;
+
+        if needle == haystack[mid] {
+            return Some(mid);
+        }else if needle < haystack[mid] {
+            return recursive_bin_search(haystack, needle, low , mid-1);
+        }else{
+            return recursive_bin_search(haystack, needle, mid+1, high);
+        }
+    }
     None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use crate::binary_search::recursive_bin_search;
 use crate::linear_search::lin_search;
 
 mod linear_search;
@@ -11,8 +11,32 @@ fn main() {
 
     let haystack = vec!["abs", "bcd", "dsa", "cde"];
     let needle = "dsa";
+    let pos2 = recursive_bin_search(&haystack, needle, 0, haystack.len()-1).unwrap();
 
     let position = lin_search(&haystack, needle).unwrap();
     println!("Target {} found at index: {}",needle, position);
+    println!("Target {} found at index: {}",needle, pos2);
 
+
+
+}
+
+mod questions {
+    pub fn two_crystal_balls(){
+        /*
+        When given two crystal balls that will break if dropped from a
+        high enough distance, determine the exact spot in which it will break
+        in the most optimized way.
+        */
+
+        //false values upto a point true values follow
+        //[false, false, false, false, false, true, true, true, ...]
+        //move to the 1/2^N 
+        let low = false;
+        let high = true;
+
+
+
+
+    }
 }
