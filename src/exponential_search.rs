03213eed9363badfb60a2cef9f64d59a0dd12b2d
Hezekiah Munene <hezemunene01@gmail.com>
2024-02-21 22:03:24 +0300

    implemented an exponential search function

---
+++ src/exponential_search.rs
@@ -0,0 +1,32 @@
+use std::cmp::min;
+use crate::binary_search::recursive_bin_search;
+
+pub fn exponen_search<T>(haystack:&[T], needle: T) -> Option<usize>
+where T: Copy + Ord
+{
+    let elements = haystack.len() ;
+    let mut index = 1;
+
+    if haystack[0] == needle {
+        return Some(0);
+    }else {
+        while index < elements && haystack[index] <= needle {
+            //{1,2,3,4,5} needle 5
+            index *= 2;
+        }
+        return recursive_bin_search(&haystack, needle, index /2 , min(index, elements));
+    }
+
+}
+
+
+/*
+ //{1,2,3,4,5,6} needle 5
+l = 5
+i = 1 true -> i <  l , a[i] <= t
+ i = 2 true -> i < l , a[i] <= t
+ i = 4 true -> i < n (4 < 5 ) , a[i] <= t
+ i = 8 f -> i < n (8 < 5) , a[i] <= t
+  case f
+    recur_bin_search(a, i/2(4), min(i(8) , l(5)), t)
+ */
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use crate::binary_search::recursive_bin_search;
+use crate::exponential_search::exponen_search;
 use crate::linear_search::lin_search;
 
 mod linear_search;
 mod binary_search;
+mod exponential_search;
 
 fn main() {
     // let data = vec!['a', 'b', 'c'];
     // let target = 'c';
     // let answer = bin_search(&data, target).unwrap();
 
-    let haystack = vec!["abs", "bcd", "dsa", "cde"];
+    let haystack = vec!["abs", "bcd", "cde", "dsa"];
     let needle = "dsa";
     let pos2 = recursive_bin_search(&haystack, needle, 0, haystack.len()-1).unwrap();
+    let po3  = exponen_search(&haystack, needle).unwrap();
 
     let position = lin_search(&haystack, needle).unwrap();
     println!("Target {} found at index: {}",needle, position);
     println!("Target {} found at index: {}",needle, pos2);
+    println!("Target {} found at index: {}",needle, po3);
+
 
 
 
